e5c53e9bad041fe4fb12c56c63ce9d47e18eac1d
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-09-29 15:31:24 -0400

    Created a ReviewController.cs file

---
+++ eCommerceStarterCode/Controllers/ReviewController.cs
@@ -0,0 +1,47 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ReviewController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllReviews()
+        {
+            var products = _context.Reviews;
+            return Ok(products);
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetSingleReview(int Id)
+        {
+            var review = _context.Reviews.Find(Id);
+            return Ok(review);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Review value)
+        {
+            _context.Reviews.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+
+    }
+}
+    
+
