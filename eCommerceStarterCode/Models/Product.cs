f78edba2a66e0920e864a14b745bf3b03a535d99
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-09-29 11:39:45 -0400

    Updated Product.cs and deleted CustomerController and Customer model.

--- eCommerceStarterCode/Models/Product.cs
+++ eCommerceStarterCode/Models/Product.cs
@@ -6,6 +6,7 @@ namespace eCommerceStarterCode.Models
     {
         public int    ProductId { get; set; }
         public string ProductName { get; set; }
+        public string Artist { get; set; }
         public float  Price { get; set; }
         public string Description { get; set; }
         public string Genre { get; set; }
